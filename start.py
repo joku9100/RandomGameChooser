fad8c3045a3657f5f844fff117793dfa84c856f9
joku9100 <josephtiemansemail@gmail.com>
2015-06-03 23:19:46 -0400

    message

--- start.py
+++ start.py
@@ -2,4 +2,9 @@ hello = "hi"
 gameinput = raw_data("Input games here" )
 list_games = [];
 append.list_games(gameinput)
-
+while gameinput != "0":
+    gameinput = raw_data("Input additional games.  Type 0 to exit")
+    append.list_games(gameinput)
+    
+    
+    
