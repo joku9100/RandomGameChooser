8c05cc4ebd4dba3e58055bd3d8023977de606778
joku9100 <josephtiemansemail@gmail.com>
2015-06-04 23:18:24 -0400

    message

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ import os
 import pickle
 
 def randomizegame():
+   # print (list_games)
     amount_games = len(list_games)
     print (list_games[random.randrange(0,amount_games,1)])
 
@@ -16,6 +17,7 @@ while 1:
     try:
         list_games.append(pickle.load(f))
     except EOFError:
+        f.close()
         break
 
 
@@ -24,19 +26,37 @@ answer  = input( "Do you want to add more games? Yes or No ")
 gameinput = 'what'
 if answer == 'Yes':
     while gameinput != "0":
+        gameindata = True
         print("Enter 0 to exit")
         gameinput = input("Input additional games: ")
-        
-        while
-        if gameinput == list_games[len(list_games-1)]
-        list_games.append(gameinput)
+        #list_games.append(gameinput)
+        a = 0
+        if gameinput!='0':
+            while a +1 < len(list_games):
+                if gameinput == list_games[a]:
+                    gameindata = False
+                    
+                a = a +1
+            if gameindata:
+                list_games.append(gameinput)
+                
 
 
 
 
-answer  = input( "Do you want to pick a game? Yes or No")
+answer  = input( "Do you want to pick a game? Yes or No ")
 
 if answer == 'Yes':
     randomizegame()
+
+
+f = open('gamedata.txt','wb')
+games=len(list_games)
+a = 0
+while a   <games:
+    pickle.dump(list_games[games-1],f)
+    list_games.pop()
+    games = len(list_games)
+    
 
 
